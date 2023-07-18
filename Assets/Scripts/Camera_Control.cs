60b82d9c4273126d02ecd83dcb9be2e308f7d2e0
Maurice Davis <123034869+CodedMD@users.noreply.github.com>
2023-07-17 18:26:18 -0700

    Added a Camera Control and input system to control the view

---
+++ Assets/Scripts/Camera_Control.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Control : MonoBehaviour
+{
+
+    [SerializeField] GameObject[] _vCams;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _vCams[0].SetActive(false);
+            _vCams[1].SetActive(false);
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            _vCams[0].SetActive(true);
+            _vCams[1].SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            _vCams[2].SetActive(false);
+            _vCams[3].SetActive(false);
+        }
+        if (Input.GetKeyUp(KeyCode.C))
+        {
+            _vCams[2].SetActive(true);
+            _vCams[3].SetActive(true);
+        }
+    }
+}
